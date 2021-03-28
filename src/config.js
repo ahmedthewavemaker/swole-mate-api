1493e60cd076213929452b89e32b5ff81305884f
Ahmed Elnasseh <elnassehag@vcu.edu>
2021-03-28 19:22:08 -0400

    hi

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL || "http://localhost:8000",
     CLIENT_ORIGIN: 'https://swole-mate-app.vercel.app/',
-    DATABASE_URL:'postgresql://dunder_mifflin@localhost/swole-mate',
+    DATABASE_URL:process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/swole-mate',
     TEST_DATABASE_URL:'postgresql://dunder_mifflin@localhost/swole-mate-test',
   }
